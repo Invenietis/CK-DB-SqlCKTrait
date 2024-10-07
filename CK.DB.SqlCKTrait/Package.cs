7ea13a233f28c02daba8b86aae61a344a52d78d5
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-10-07 08:24:10 +0200

    Applying Code Cleanup.

--- CK.DB.SqlCKTrait/Package.cs
+++ CK.DB.SqlCKTrait/Package.cs
@@ -1,24 +1,23 @@
 using CK.Core;
 
-namespace CK.DB.SqlCKTrait
+namespace CK.DB.SqlCKTrait;
+
+/// <summary>
+/// Package that supports <see cref="CKTraitContextTable"/> and <see cref="CKTraitTable"/> sql mapping. 
+/// </summary>
+[SqlPackage( Schema = "CK", ResourcePath = "Res" )]
+[Versions( "1.0.0" )]
+public abstract class Package : SqlPackage
 {
     /// <summary>
-    /// Package that supports <see cref="CKTraitContextTable"/> and <see cref="CKTraitTable"/> sql mapping. 
+    /// Gets the <see cref="CKTraitContextTable"/>.
     /// </summary>
-    [SqlPackage( Schema = "CK", ResourcePath = "Res" )]
-    [Versions("1.0.0")]
-    public abstract class Package : SqlPackage
-    {
-        /// <summary>
-        /// Gets the <see cref="CKTraitContextTable"/>.
-        /// </summary>
-        [InjectObject]
-        public CKTraitContextTable CKTraitContextTable { get; private set; }
+    [InjectObject]
+    public CKTraitContextTable CKTraitContextTable { get; private set; }
 
-        /// <summary>
-        /// Gets the <see cref="CKTraitTable"/>.
-        /// </summary>
-        [InjectObject]
-        public CKTraitTable CKTraitTable { get; private set; }
-    }
+    /// <summary>
+    /// Gets the <see cref="CKTraitTable"/>.
+    /// </summary>
+    [InjectObject]
+    public CKTraitTable CKTraitTable { get; private set; }
 }
--- Tests/CK.DB.SqlCKTrait.Tests/RawTraitTests.cs
+++ Tests/CK.DB.SqlCKTrait.Tests/RawTraitTests.cs
@@ -9,123 +9,122 @@ using Microsoft.Data.SqlClient;
 using System.Data;
 using CK.Testing;
 
-namespace CK.DB.SqlCKTrait.Tests
+namespace CK.DB.SqlCKTrait.Tests;
+
+[TestFixture]
+public class RawTraitTests
 {
-    [TestFixture]
-    public class RawTraitTests
+    [Test]
+    public void proper_subsets_and_supersets()
     {
-        [Test]
-        public void proper_subsets_and_supersets()
+        var p = SharedEngine.Map.StObjs.Obtain<Package>();
+        using( var ctx = new SqlStandardCallContext() )
         {
-            var p = SharedEngine.Map.StObjs.Obtain<Package>();
-            using( var ctx = new SqlStandardCallContext() )
+            int contextId = p.CKTraitContextTable.RegisterContext( ctx, 1, "RawTest", ',' );
+            int letterId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "A,B,C,D,E,F" );
+            int digitId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "0,1,2,3,4,5,6,7,8,9" );
+            int evenDigitId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "0,2,4,6,8" );
+            int oddDigitId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "1,3,5,7,9" );
+            int[] digitsId = Enumerable.Range( 0, 10 )
+                                .Select( i => p.CKTraitTable.FindOrCreate( ctx, 1, contextId, findOnly: true, traitName: i.ToString() ) )
+                                .ToArray();
+            digitsId.Should().Contain( id => id > 0 );
+
+            using( var cmdSubSet = new SqlCommand( "select CKTraitId from CK.fCKTraitProperSubSet( @Id )" ) )
+            using( var cmdSuperSet = new SqlCommand( "select CKTraitId from CK.fCKTraitProperSuperSet( @Id )" ) )
             {
-                int contextId = p.CKTraitContextTable.RegisterContext( ctx, 1, "RawTest", ',' );
-                int letterId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "A,B,C,D,E,F" );
-                int digitId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "0,1,2,3,4,5,6,7,8,9" );
-                int evenDigitId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "0,2,4,6,8" );
-                int oddDigitId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "1,3,5,7,9" );
-                int[] digitsId = Enumerable.Range( 0, 10 )
-                                    .Select( i => p.CKTraitTable.FindOrCreate( ctx, 1, contextId, findOnly: true, traitName: i.ToString() ) )
-                                    .ToArray();
-                digitsId.Should().Contain( id => id > 0 );
+                var pSubSet = cmdSubSet.Parameters.Add( "@Id", SqlDbType.Int );
+                var pSuperSet = cmdSuperSet.Parameters.Add( "@Id", SqlDbType.Int );
 
-                using( var cmdSubSet = new SqlCommand( "select CKTraitId from CK.fCKTraitProperSubSet( @Id )" ) )
-                using( var cmdSuperSet = new SqlCommand( "select CKTraitId from CK.fCKTraitProperSuperSet( @Id )" ) )
+                pSuperSet.Value = evenDigitId;
+                ctx[p].ExecuteReader( cmdSuperSet, row => row.GetInt32( 0 ) )
+                      .Should().BeEquivalentTo( new[] { digitId } );
+
+                pSuperSet.Value = oddDigitId;
+                ctx[p].ExecuteReader( cmdSuperSet, row => row.GetInt32( 0 ) )
+                      .Should().BeEquivalentTo( new[] { digitId } );
+
+                pSubSet.Value = digitId;
+                ctx[p].ExecuteReader( cmdSubSet, row => row.GetInt32( 0 ) )
+                      .Should().BeEquivalentTo( new[] { evenDigitId, oddDigitId }.Concat( digitsId ) );
+
+                for( int idx = 0; idx < 10; ++idx )
                 {
-                    var pSubSet = cmdSubSet.Parameters.Add( "@Id", SqlDbType.Int );
-                    var pSuperSet = cmdSuperSet.Parameters.Add( "@Id", SqlDbType.Int );
-
-                    pSuperSet.Value = evenDigitId;
-                    ctx[p].ExecuteReader( cmdSuperSet, row => row.GetInt32( 0 ) )
-                          .Should().BeEquivalentTo( new[] { digitId } );
-
-                    pSuperSet.Value = oddDigitId;
-                    ctx[p].ExecuteReader( cmdSuperSet, row => row.GetInt32( 0 ) )
-                          .Should().BeEquivalentTo( new[] { digitId } );
-
-                    pSubSet.Value = digitId;
-                    ctx[p].ExecuteReader( cmdSubSet, row => row.GetInt32( 0 ) )
-                          .Should().BeEquivalentTo( new[] { evenDigitId, oddDigitId }.Concat( digitsId ) );
-
-                    for( int idx = 0; idx < 10; ++idx )
+                    var atomId = digitsId[idx];
+                    pSuperSet.Value = atomId;
+                    List<int> superSets = ctx[p].ExecuteReader( cmdSuperSet, row => row.GetInt32( 0 ) );
+                    if( idx % 2 == 0 )
                     {
-                        var atomId = digitsId[idx];
-                        pSuperSet.Value = atomId;
-                        List<int> superSets = ctx[p].ExecuteReader( cmdSuperSet, row => row.GetInt32( 0 ) );
-                        if( idx % 2 == 0 )
-                        {
-                            superSets.Should().BeEquivalentTo( new[] { evenDigitId, digitId } );
-                        }
-                        else
-                        {
-                            superSets.Should().BeEquivalentTo( new[] { oddDigitId, digitId } );
-                        }
-                        // Since these are atomic trait, there is no SubSet.
-                        pSubSet.Value = atomId;
-                        ctx[p].ExecuteReader( cmdSubSet, row => row.GetInt32( 0 ) ).Should().BeEmpty();
+                        superSets.Should().BeEquivalentTo( new[] { evenDigitId, digitId } );
                     }
-
+                    else
+                    {
+                        superSets.Should().BeEquivalentTo( new[] { oddDigitId, digitId } );
+                    }
+                    // Since these are atomic trait, there is no SubSet.
+                    pSubSet.Value = atomId;
+                    ctx[p].ExecuteReader( cmdSubSet, row => row.GetInt32( 0 ) ).Should().BeEmpty();
                 }
+
             }
         }
+    }
 
-        [Test]
-        public void subsets_and_supersets()
+    [Test]
+    public void subsets_and_supersets()
+    {
+        var p = SharedEngine.Map.StObjs.Obtain<Package>();
+        using( var ctx = new SqlStandardCallContext() )
         {
-            var p = SharedEngine.Map.StObjs.Obtain<Package>();
-            using( var ctx = new SqlStandardCallContext() )
+            int contextId = p.CKTraitContextTable.RegisterContext( ctx, 1, "RawTest", ',' );
+            int letterId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "A,B,C,D,E,F" );
+            int digitId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "0,1,2,3,4,5,6,7,8,9" );
+            int evenDigitId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "0,2,4,6,8" );
+            int oddDigitId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "1,3,5,7,9" );
+            int[] digitsId = Enumerable.Range( 0, 10 )
+                                .Select( i => p.CKTraitTable.FindOrCreate( ctx, 1, contextId, findOnly: true, traitName: i.ToString() ) )
+                                .ToArray();
+            digitsId.Should().Contain( id => id > 0 );
+
+            using( var cmdSubSet = new SqlCommand( "select CKTraitId from CK.fCKTraitSubSet( @Id )" ) )
+            using( var cmdSuperSet = new SqlCommand( "select CKTraitId from CK.fCKTraitSuperSet( @Id )" ) )
             {
-                int contextId = p.CKTraitContextTable.RegisterContext( ctx, 1, "RawTest", ',' );
-                int letterId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "A,B,C,D,E,F" );
-                int digitId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "0,1,2,3,4,5,6,7,8,9" );
-                int evenDigitId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "0,2,4,6,8" );
-                int oddDigitId = p.CKTraitTable.FindOrCreate( ctx, 1, contextId, false, "1,3,5,7,9" );
-                int[] digitsId = Enumerable.Range( 0, 10 )
-                                    .Select( i => p.CKTraitTable.FindOrCreate( ctx, 1, contextId, findOnly: true, traitName: i.ToString() ) )
-                                    .ToArray();
-                digitsId.Should().Contain( id => id > 0 );
+                var pSubSet = cmdSubSet.Parameters.Add( "@Id", SqlDbType.Int );
+                var pSuperSet = cmdSuperSet.Parameters.Add( "@Id", SqlDbType.Int );
 
-                using( var cmdSubSet = new SqlCommand( "select CKTraitId from CK.fCKTraitSubSet( @Id )" ) )
-                using( var cmdSuperSet = new SqlCommand( "select CKTraitId from CK.fCKTraitSuperSet( @Id )" ) )
+                pSuperSet.Value = evenDigitId;
+                ctx[p].ExecuteReader( cmdSuperSet, row => row.GetInt32( 0 ) )
+                      .Should().BeEquivalentTo( new[] { digitId, evenDigitId } );
+
+                pSuperSet.Value = oddDigitId;
+                ctx[p].ExecuteReader( cmdSuperSet, row => row.GetInt32( 0 ) )
+                      .Should().BeEquivalentTo( new[] { digitId, oddDigitId } );
+
+                pSubSet.Value = digitId;
+                ctx[p].ExecuteReader( cmdSubSet, row => row.GetInt32( 0 ) )
+                      .Should().BeEquivalentTo( new[] { evenDigitId, oddDigitId, digitId }.Concat( digitsId ) );
+
+                for( int idx = 0; idx < 10; ++idx )
                 {
-                    var pSubSet = cmdSubSet.Parameters.Add( "@Id", SqlDbType.Int );
-                    var pSuperSet = cmdSuperSet.Parameters.Add( "@Id", SqlDbType.Int );
-
-                    pSuperSet.Value = evenDigitId;
-                    ctx[p].ExecuteReader( cmdSuperSet, row => row.GetInt32( 0 ) )
-                          .Should().BeEquivalentTo( new[] { digitId, evenDigitId } );
-
-                    pSuperSet.Value = oddDigitId;
-                    ctx[p].ExecuteReader( cmdSuperSet, row => row.GetInt32( 0 ) )
-                          .Should().BeEquivalentTo( new[] { digitId, oddDigitId } );
-
-                    pSubSet.Value = digitId;
-                    ctx[p].ExecuteReader( cmdSubSet, row => row.GetInt32( 0 ) )
-                          .Should().BeEquivalentTo( new[] { evenDigitId, oddDigitId, digitId }.Concat( digitsId ) );
-
-                    for( int idx = 0; idx < 10; ++idx )
+                    var atomId = digitsId[idx];
+                    pSuperSet.Value = atomId;
+                    List<int> superSets = ctx[p].ExecuteReader( cmdSuperSet, row => row.GetInt32( 0 ) );
+                    if( idx % 2 == 0 )
                     {
-                        var atomId = digitsId[idx];
-                        pSuperSet.Value = atomId;
-                        List<int> superSets = ctx[p].ExecuteReader( cmdSuperSet, row => row.GetInt32( 0 ) );
-                        if( idx % 2 == 0 )
-                        {
-                            superSets.Should().BeEquivalentTo( new[] { atomId, evenDigitId, digitId } );
-                        }
-                        else
-                        {
-                            superSets.Should().BeEquivalentTo( new[] { atomId, oddDigitId, digitId } );
-                        }
-                        // Since these are atomic trait, they are necessaily alone.
-                        pSubSet.Value = atomId;
-                        ctx[p].ExecuteReader( cmdSubSet, row => row.GetInt32( 0 ) ).Should().BeEquivalentTo( new[] { atomId } );
+                        superSets.Should().BeEquivalentTo( new[] { atomId, evenDigitId, digitId } );
                     }
-
+                    else
+                    {
+                        superSets.Should().BeEquivalentTo( new[] { atomId, oddDigitId, digitId } );
+                    }
+                    // Since these are atomic trait, they are necessaily alone.
+                    pSubSet.Value = atomId;
+                    ctx[p].ExecuteReader( cmdSubSet, row => row.GetInt32( 0 ) ).Should().BeEquivalentTo( new[] { atomId } );
                 }
 
-
             }
+
+
         }
     }
 }
